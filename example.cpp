a127eaa7db8fd548dbc021f9e8a5ae5b55b24871
prashanth <prashanthbandi0319@gmail.com>
2025-07-13 17:49:40 +0530

    this is my cpp file

---
+++ example.cpp
@@ -0,0 +1,1 @@
+this is cpp program file
---
+++ program.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Header for input/output stream
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print to console
+    return 0;  // Exit code
+}
